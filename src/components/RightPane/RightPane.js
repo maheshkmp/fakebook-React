4059a05d3ca5a1727fd388a5070e8a57874bf243
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-09-10 00:31:57 +0530

    front-end completed mostly

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -12,6 +12,77 @@ export default function RightPane() {
             Order your food with Uber delivery service. We can deliver your food into your door-step within seconds. Order now!!!
           </span>
         </div>
+        <div className="birthDayContainer">
+          <img src="/images/birthday.png" alt="" className="birthdayImage" />
+          <span className="birthdaytext">
+            <b>Kokila gihan</b> and <b>2 others</b> having birthday today..!
+            </span>
+        </div>
+        <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/images/person.png" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFreindName">Sirimal kurumaniya</span>
+          </li>
+        </div>
       </div>
     </div>
   )
